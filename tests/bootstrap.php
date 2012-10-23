668fde2a0ea9c5e47371a532889a888e503158d7
Alex Shelkovskiy <alexshelkov@gmail.com>
2012-10-23 18:25:03 +0700

    Change path to relative.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ chdir($applicationRoot);
 // Init composer autoloaders
 $loader = require_once __DIR__ . '/../vendor/autoload.php';
 $loader->add('ApptSimpleAuthTest\\', __DIR__ );
-$loader->add('DoctrineMongoODMModuleTest',  '/Users/san/Sites/myprojects/ApptSimpleAuth/vendor/doctrine/doctrine-mongo-odm-module/tests');
+$loader->add('DoctrineMongoODMModuleTest',  __DIR__  . '/../vendor/doctrine/doctrine-mongo-odm-module/tests');
 
 $config = include(__DIR__ . '/test.application.config.php');
 
